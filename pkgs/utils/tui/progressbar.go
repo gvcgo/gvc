41d29ca79110b86f32ecc7b01bf7a0d60db6e947
moqsien <moqsien@foxmail.com>
2023-05-17 10:20:37 +0800

    progressbar

--- pkgs/utils/tui/progressbar.go
+++ pkgs/utils/tui/progressbar.go
@@ -29,18 +29,26 @@ func NewProgressBar(filename string, length int) (p *ProgressBar) {
 		ContentLength: length,
 		Filename:      filename,
 	}
-	p.Bar = pterm.DefaultProgressbar.WithTitle(fmt.Sprintf("Downloading <%s %sMB>", filename, p.calcSize())).WithTotal(BarLength).WithShowCount(false).WithShowPercentage(true)
+	total, current := p.calcSize()
+	p.Bar = pterm.DefaultProgressbar.WithTitle(fmt.Sprintf("Downloading <%s %s/%sMB>", filename, current, total)).WithTotal(BarLength).WithShowCount(false).WithShowPercentage(true)
 	p.divideBy = length / BarLength
 	return
 }
 
-func (that *ProgressBar) calcSize() string {
+func (that *ProgressBar) calcSize() (total, current string) {
 	mb := float64(that.ContentLength) / float64(1024*1024)
-	return fmt.Sprintf("%.2f", mb)
+	total = fmt.Sprintf("%.2f", mb)
+	length := len(total) - 2
+	mb = float64(that.CurrentReceived) / float64(1024*1024)
+	current = fmt.Sprintf("%"+fmt.Sprintf("%v", length)+".2f", mb)
+	return
 }
 
 func (that *ProgressBar) Write(p []byte) (n int, err error) {
 	n = len(p)
+	if that.CurrentReceived == 0 {
+		that.Bar.Increment()
+	}
 	that.CurrentReceived += n
 	var increasement int
 	if that.CurrentReceived == that.ContentLength {
@@ -48,9 +56,8 @@ func (that *ProgressBar) Write(p []byte) (n int, err error) {
 	} else {
 		increasement = (that.CurrentReceived / that.divideBy) - that.Bar.Current
 	}
-	if that.CurrentReceived == that.ContentLength {
-		that.Bar.UpdateTitle(fmt.Sprintf("Downloading <%s %sMB>", that.Filename, that.calcSize()))
-	}
+	total, current := that.calcSize()
+	that.Bar.UpdateTitle(fmt.Sprintf("Downloading <%s %s/%sMB>", that.Filename, current, total))
 	if increasement > 0 {
 		that.Bar.Add(increasement)
 	}
