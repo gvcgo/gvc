4dac7ca6e71f2d5e583ac882d1900e466afb5e4c
moqsien <moqsien@foxmail.com>
2024-03-25 22:12:12 +0800

    proxy for git ssh

--- cmd/git.go
+++ cmd/git.go
@@ -14,8 +14,8 @@ func RegisterGit(cli *Cli) {
 	}
 
 	hosts := &cobra.Command{
-		Use:     "hosts",
-		Aliases: []string{"h"},
+		Use:     "update-hosts",
+		Aliases: []string{"uh", "u"},
 		Short:   "Updates hosts file.",
 		Run: func(cmd *cobra.Command, args []string) {
 			m := git.NewModifier()
@@ -23,6 +23,42 @@ func RegisterGit(cli *Cli) {
 		},
 	}
 	parent.AddCommand(hosts)
+
+	var (
+		destHostName string = "dest_host"
+		destPortName string = "dest_port"
+		timeoutName  string = "timeout"
+	)
+	crokscrew := &cobra.Command{
+		Use:     "crokscrew",
+		Aliases: []string{"cs", "c"},
+		Short:   "Http proxy for ssh.",
+		Long:    "Example: g g cs --dest_host=xxx --dest_port=xxx --timeout=xxx",
+		Run: func(cmd *cobra.Command, args []string) {
+			destHost, _ := cmd.Flags().GetString(destHostName)
+			destPort, _ := cmd.Flags().GetString(destPortName)
+			timeout, _ := cmd.Flags().GetInt(timeoutName)
+			if destHost == "" || destPort == "" {
+				cmd.Help()
+				return
+			}
+			git.GrokscrewHttpSSH(destHost, destPort, timeout)
+		},
+	}
+	crokscrew.Flags().StringP(destHostName, "a", "", "Specifies dest host.")
+	crokscrew.Flags().StringP(destPortName, "p", "", "Specifies dest port.")
+	crokscrew.Flags().IntP(timeoutName, "t", 10, "Specifies timeout.")
+	parent.AddCommand(crokscrew)
+
+	toggle := &cobra.Command{
+		Use:     "toggle-proxy",
+		Aliases: []string{"tp", "t"},
+		Short:   "Toggle proxy for git ssh.",
+		Run: func(cmd *cobra.Command, args []string) {
+			git.ToggleProxyForSSH()
+		},
+	}
+	parent.AddCommand(toggle)
 
 	cli.rootCmd.AddCommand(parent)
 }
--- pkg/git/ssh.go
+++ pkg/git/ssh.go
@@ -1,5 +1,149 @@
 package git
 
+import (
+	"fmt"
+	"net/url"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/gvcgo/goutils/pkgs/gtea/gprint"
+	"github.com/gvcgo/goutils/pkgs/gutils"
+	"github.com/gvcgo/gvc/conf"
+)
+
+var (
+	GitSSHProxyCommandWin  string = `ProxyCommand connect -S %s %s %s`
+	GitSSHProxyCommandNix  string = `ProxyCommand nc -v -x %s %s %s`
+	GitSSHProxyCommandHttp string = `ProxyCommand g g cs -a %s -p %s`
+)
+
+var GitSSHConfigStr string = `Host github.com
+  User git
+  Port 22
+  Hostname github.com
+  IdentityFile "%s"
+  TCPKeepAlive yes
+  %s
+
+Host ssh.github.com
+  User git
+  Port 443
+  Hostname ssh.github.com
+  IdentityFile "%s"
+  TCPKeepAlive yes
+  %s
+`
+
 /*
 Adds proxy to hosts file.
 */
+func SetProxyForSSH() {
+	cfg := conf.NewGVConfig()
+	pURI := cfg.GetLocalProxy()
+	if pURI == "" {
+		gprint.PrintError("No legal proxy is specified.")
+		return
+	}
+	u, err := url.Parse(pURI)
+	if err != nil {
+		return
+	}
+	homeDir, _ := os.UserHomeDir()
+	dotSSHPath := filepath.Join(homeDir, ".ssh")
+	idRSAPath := filepath.Join(dotSSHPath, "id_rsa")
+	if ok, _ := gutils.PathIsExist(idRSAPath); !ok {
+		gprint.PrintError("Cannot find ~/.ssh/id_rsa.")
+		return
+	}
+	uStr := fmt.Sprintf("%s:%s", u.Hostname(), u.Port())
+	pxyCmd := ""
+
+	switch runtime.GOOS {
+	case gutils.Windows:
+		if strings.Contains(u.Scheme, "sock") {
+			pxyCmd = fmt.Sprintf(
+				GitSSHProxyCommandWin,
+				uStr,
+				`%h`,
+				`%p`,
+			)
+		} else {
+			pxyCmd = fmt.Sprintf(
+				GitSSHProxyCommandHttp,
+				`%h`,
+				`%p`,
+			)
+		}
+	case gutils.Linux, gutils.Darwin:
+		if strings.Contains(u.Scheme, "sock") {
+			pxyCmd = fmt.Sprintf(
+				GitSSHProxyCommandNix,
+				uStr,
+				`%h`,
+				`%p`,
+			)
+		} else {
+			pxyCmd = fmt.Sprintf(
+				GitSSHProxyCommandHttp,
+				`%h`,
+				`%p`,
+			)
+		}
+	default:
+		gprint.PrintError("Unsupported OS.")
+		return
+	}
+
+	content := fmt.Sprintf(
+		GitSSHConfigStr,
+		idRSAPath,
+		pxyCmd,
+		idRSAPath,
+		pxyCmd,
+	)
+	setProxyForSSH(dotSSHPath, content)
+}
+
+func setProxyForSSH(dotSSHPath, content string) {
+	confPath := filepath.Join(dotSSHPath, "config")
+	if ok, _ := gutils.PathIsExist(confPath); !ok {
+		os.WriteFile(confPath, []byte(content), 0o666)
+	} else {
+		oldContentByte, _ := os.ReadFile(confPath)
+		oldContent := string(oldContentByte)
+		if !strings.Contains(oldContent, "ProxyCommand") && len(oldContent) > 0 {
+			os.WriteFile(confPath, []byte(oldContent+"\n"+content), os.ModePerm)
+		} else {
+			os.WriteFile(confPath, []byte(content), 0o666)
+		}
+	}
+}
+
+func ToggleProxyForSSH() {
+	homeDir, _ := os.UserHomeDir()
+	confPath := filepath.Join(homeDir, ".ssh", "config")
+	backupConfPath := filepath.Join(homeDir, ".ssh", "config.bak")
+
+	ok1, _ := gutils.PathIsExist(confPath)
+	ok2, _ := gutils.PathIsExist(backupConfPath)
+
+	if !ok1 && !ok2 {
+		gprint.PrintWarning("No proxy available. Please set a proxy for ssh.")
+		SetProxyForSSH()
+	} else if ok1 && !ok2 {
+		if err := os.Rename(confPath, backupConfPath); err == nil {
+			gprint.PrintInfo("Proxy disabled.")
+		}
+	} else if !ok1 && ok2 {
+		if err := os.Rename(backupConfPath, confPath); err == nil {
+			gprint.PrintSuccess("Proxy enabled.")
+		}
+	} else {
+		os.RemoveAll(backupConfPath)
+		if err := os.Rename(confPath, backupConfPath); err == nil {
+			gprint.PrintInfo("Proxy disabled.")
+		}
+	}
+}
